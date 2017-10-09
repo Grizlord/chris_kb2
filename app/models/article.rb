22088915ec5090c503be79f5d1b54ad327869f67
Chris <chrisdow38@gmail.com>
2017-10-09 18:19:22 +0000

    minor fixes

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :category_id, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :content, presence: true
   
   include PgSearch
