afe3117ea110e5e5b3218209e34627bb1e0f1cd6
Chris <chrisdow38@gmail.com>
2017-10-17 16:14:41 +0000

    admin check box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :show]
   before_action :admin_user,     only: :destroy
   
   def show
@@ -51,14 +51,14 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :admin)
     end
     
     # Before filters
 
-   # Confirms the correct user.
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) && flash[:danger] = "You are not authorized!" unless current_user?(@user)
     end
 end
