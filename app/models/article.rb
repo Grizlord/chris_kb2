2c9559d929c8456c6f9d4422e18d0703029a1e73
Chris <chrisdow38@gmail.com>
2017-11-09 21:55:12 +0000

    clean up flash messages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates :title, presence: true, uniqueness: { case_sensitive: false }
